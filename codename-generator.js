9831d86b446ede128e923221e9fca4dd47ba27f9
Mark Murphy <mark.murphy@techemail.com>
2019-10-12 18:19:47 +0100

    Basic Functionality

--- codename-generator.js
+++ codename-generator.js
@@ -1,9 +1,30 @@
+
 function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
 }
 
-// Main
+// Main()
+try {
+    // *** TO DO --> Read command line arguments looking for --version, --help and iterations
 
-for (let i =0; i < 50; i++) {
-    console.log(randomRange(0, 1000));
+    // Default number of codewords to generate
+    var iterations = 1;
+
+    const fs = require('fs');
+    // Read list of adjectives and nouns
+    const adjectives = JSON.parse(fs.readFileSync('adjectives.json'));
+    const nouns = JSON.parse(fs.readFileSync('nouns.json'));
+
+    for (let i = 0; i < iterations; i++) {
+        // Get random adjective
+        var adjective = adjectives[randomRange(0, adjectives.length)];
+        // Get random nouns
+        var noun = nouns[randomRange(0, nouns.length)];
+
+        // Output code name
+        console.log('%s %s', adjective, noun);
+    }
+
+} catch (error) {
+    console.error('An error occurred: %O', error);
 }
