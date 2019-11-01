83068e8ccb3ceeebe1d1924803c60b16ebc1d87a
Mark Murphy <mark.murphy@techemail.com>
2019-11-01 15:20:02 +0000

    Command Line options support

--- codename-generator.js
+++ codename-generator.js
@@ -1,3 +1,7 @@
+// command line options parser
+var argv = require('yargs')
+.help(false)
+.argv;
 
 function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
@@ -6,13 +10,25 @@ function randomRange(myMin, myMax) {
 // Main()
 try {
     // *** TO DO --> Read command line arguments looking for --version, --help and iterations
+    // Check for 'help' command line parameters
+    if (argv.help) {
+        // Show help screen
+        const help = require('./help');
+        help.helpScreen(argv.verbose);
+        return;
+    }
 
     // Default number of codewords to generate
     var iterations = 1;
 
+    // Check if a specific number was provided
+    if (Number.isInteger(Number(process.argv[2]))) {
+        // Update iterations
+        iterations = Number(process.argv[2]);
+    }
+
     const fs = require('fs');
     // Read list of adjectives and nouns
-    // *** TO DO --> Get absolute file paths via __dirname
     const adjectives = JSON.parse(fs.readFileSync(__dirname + '/adjectives.json'));
     const nouns = JSON.parse(fs.readFileSync(__dirname + '/nouns.json'));
 
