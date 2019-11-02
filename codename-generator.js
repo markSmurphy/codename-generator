ed4bdad7d6b493280e7268a84951231f3b34214c
Mark Murphy <mark.murphy@techemail.com>
2019-11-02 12:43:57 +0000

    Implemented debug logging

--- codename-generator.js
+++ codename-generator.js
@@ -1,3 +1,6 @@
+const debug = require('debug')('codename-generator');
+debug('Entry: [%s]', __filename);
+
 // command line options parser
 var argv = require('yargs')
 .help(false)
@@ -6,14 +9,18 @@ var argv = require('yargs')
 // console colours
 const chalk = require('chalk');
 
-function randomRange(myMin, myMax) {
-    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+function randomRange(minimum, maximum) {
+    debug('randomRange(minimum = %s, maximum = %s)', minimum, maximum);
+    var randomNumber =Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
+    debug('Randomly generated: %s', randomNumber);
+    return(randomNumber);
 }
 
 // Main()
 try {
     // Check for 'help' command line parameters
     if (argv.help) {
+        debug('--help detected.  Showing help screen.');
         // Show help screen
         const help = require('./help');
         help.helpScreen(argv.verbose);
@@ -26,9 +33,11 @@ try {
         var words = '';
         if (argv.listNouns) {
             // Read list of nouns
+            debug('--list-nouns detected');
             words = JSON.parse(fs.readFileSync(__dirname + '/nouns.json'));
         } else {
             // Read list of adjectives
+            debug('--list-adjectives detected');
             words = JSON.parse(fs.readFileSync(__dirname + '/adjectives.json'));
         }
 
@@ -45,24 +54,32 @@ try {
     // Default number of codewords to generate; The number of displayable rows in the terminal,
     // minus some room at the end so the console prompt doesn't scroll the first code name
     var iterations = process.stdout.rows - 3;
+    debug('Setting default iterations to (%s - 3) = %s', process.stdout.rows, iterations);
 
     // Check if a specific number was provided
     if (Number.isInteger(Number(process.argv[2]))) {
         // Update iterations
         iterations = Number(process.argv[2]);
+        debug('Iterations overridden to %s', iterations);
         console.log(chalk.grey('Generating %d code names...'), iterations);
     }
 
     const fs = require('fs');
     // Read list of adjectives and nouns
+    debug('Reading file: %s', __dirname + '/adjectives.json');
     const adjectives = JSON.parse(fs.readFileSync(__dirname + '/adjectives.json'));
+    debug('Reading file: %s', __dirname + '/nouns.json');
     const nouns = JSON.parse(fs.readFileSync(__dirname + '/nouns.json'));
 
     for (let i = 0; i < iterations; i++) {
+        debug('Iteration: %s', i + 1);
         // Get random adjective
         var adjective = adjectives[randomRange(0, adjectives.length)];
-        // Get random nouns
+        debug('Random Adjective: %s', adjective);
+
+        // Get random noun
         var noun = nouns[randomRange(0, nouns.length)];
+        debug('Random Noun: %s', noun);
 
         // Output code name
         console.log(chalk.bold('%s %s'), adjective, noun);
